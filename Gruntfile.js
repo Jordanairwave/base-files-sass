f56645e8babe85bdf40e08a46e12dcf9615a429b
Jordan Haines <jordan@jordanairwave.co.uk>
2015-07-28 12:36:14 +0100

    svg min added, not working tho yet
    Signed-off-by: Jordan Haines <jordan@jordanairwave.co.uk>

--- Gruntfile.js
+++ Gruntfile.js
@@ -38,6 +38,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-svgmin');
 	grunt.registerTask('default',['watch']);
 	grunt.registerTask('minify',['cssmin', 'uglify']);
 }
