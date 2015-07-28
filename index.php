6b0d7e18f6fc362b9e26f05603802194370333e7
Jordan Haines <jordan@jordanairwave.co.uk>
2015-07-28 17:06:25 +0100

    css link updated in header to point to main.css instead of style.css
    Signed-off-by: Jordan Haines <jordan@jordanairwave.co.uk>

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 	<meta name="description" content="<?php echo $pageDescription; ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-	<link href="css/style.css?v=1.0" rel="stylesheet" type="text/css" />
+	<link href="css/main.css?v=1.0" rel="stylesheet" type="text/css" />
 	<link rel="apple-touch-icon" href="apple-touch-icon.png" />
 	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon"> 
 	
