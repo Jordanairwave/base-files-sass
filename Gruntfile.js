eb474d03f6ce470ff793831f9a35e27f642dbbb3
Jordan Haines <jordan@jordanairwave.co.uk>
2015-07-29 11:51:50 +0100

    css production task added to grunt file
    Signed-off-by: Jordan Haines <jordan@jordanairwave.co.uk>

--- Gruntfile.js
+++ Gruntfile.js
@@ -93,4 +93,5 @@ module.exports = function(grunt) {
 	grunt.registerTask('default',['watch']);
 	grunt.registerTask('minify',['cssmin', 'uglify']);
 	grunt.registerTask('imageopt',['svgmin', 'imagemin']);
+	grunt.registerTask('production',['cssmin', 'uglify', 'svgmin', 'imagemin']);
 }
