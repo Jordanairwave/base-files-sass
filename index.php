e575aac88d556d2e312b6bd2a9d24715bcb297ee
Jordan Haines <jordan@factor3.co.uk>
2019-01-21 10:07:31 +0000

    Jquery updated to 3.3.1

--- index.php
+++ index.php
@@ -3,10 +3,7 @@
 	$pageDescription = 'Page description here';
 ?>
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<html>
 <head>
 	<title><?php echo $pageTitle; ?></title>
 	<meta charset="utf-8">
@@ -64,7 +61,7 @@
 			
 		</footer>
 	</div>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script defer type="text/javascript" src="js/scripts.js?v=1.0"></script>
 </body>
 
