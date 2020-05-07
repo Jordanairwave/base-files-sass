4f37963cd762136ca8c9b08a3eb22ed6b9e9789b
Jordan Haines <jordan@factor3.co.uk>
2020-05-07 10:09:05 +0100

    Jquery script changed to jquery cdn jquery-3.5.1.min.js

--- index.php
+++ index.php
@@ -55,7 +55,10 @@
 			
 		</footer>
 	</div>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script
+		src="https://code.jquery.com/jquery-3.5.1.min.js"
+		integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+		crossorigin="anonymous"></script>
 	<script defer type="text/javascript" src="js/scripts.js?v=<?php echo filemtime( 'assets/js/main-min.js' ) ?>"></script>
 </body>
 
